d7853160316f5e72d0328e96d581ce85bed46d71
thebishes <59317215+thebishes@users.noreply.github.com>
2026-06-23 18:49:05 +0100

    Delete IroningAngels-main directory
    mistakenly dropped whole website into the root folder

--- IroningAngels-main/README.md
+++
@@ -1,3 +0,0 @@
-# IroningAngels 2
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/thebishes/IroningAngels)
--- IroningAngels-main/src/types/clarity.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for Microsoft Clarity
-declare global {
-  interface Window {
-    clarity: (action: string, ...args: any[]) => void;
-  }
-}
-
-export {};
